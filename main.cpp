b96aa951f584ea92811fcd202f4d0055453d2655
Khylko Andrii <hilko_a@itstep.academy>
2024-11-04 18:31:56 +0200

    include div to the main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include"dif.h"
 #include<cmath>
 #include"prod.h"
+#include"div.h"
 using namespace std;
 
 int main()
@@ -12,4 +13,5 @@ int main()
 	cout<<12<<"-"<<23<<"="<<dif(12,23)<<endl;
 	cout<<12<<"^"<<2<<"="<<pow(12,2)<<endl;
 	cout<<12<<"*"<<23<<"="<<prod(12,23)<<endl;
+	cout<<12<<"/"<<23<<"="<<div(12,23)<<endl;
 }
